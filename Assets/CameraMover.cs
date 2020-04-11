efd541a0cf760590b432fd16895008391bf37b9d
Martyn <Martyn_Stewart89@hotmail.com>
2020-04-11 23:04:44 +0100

    S3.51 Tags and Colliders added

---
+++ Assets/CameraMover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMover : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private Transform player;
+    private Vector3 offset;
+
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player").transform;
+        offset = transform.position - player.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = (player.position + offset);
+    }
+}
--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,37 +5,71 @@ using UnityEngine;
 public class Rocket : MonoBehaviour {
 
     private Rigidbody rigidBody;
-    private AudioSource audio;
-    public float speed = 2f;
-    public float rotSpeed = 20f;
+    private AudioSource audioSource;
+    [SerializeField] private float mainthrust;
+    [SerializeField] private float rotSpeed;
 
     // Start is called before the first frame update
     void Start() {
         rigidBody = GetComponent<Rigidbody>();
-        audio = GetComponent<AudioSource>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update(){
-        ProcessInput();
+        Thrust();
+        Rotate();
     }
 
-    void ProcessInput() {
+    private void Thrust() {
         if (Input.GetKey(KeyCode.Space)) {
-            print("Space Pressed");
-            rigidBody.AddRelativeForce(Vector3.up * speed);
-            if (!audio.isPlaying) {
-                audio.Play();
+            rigidBody.AddRelativeForce(Vector3.up * mainthrust * Time.deltaTime);
+            if (!audioSource.isPlaying) {
+                audioSource.Play();
             }
         } else {
-            audio.Stop();
+            audioSource.Stop();
         }
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+
+        string tag = collision.gameObject.tag;
+
+        if (tag == "Friendly") {
+            //Rest and do nothing
+        } else if (tag == "Finish") {
+            //Run level win outcome
+            Debug.Log("A winrar is you");
+        } else {
+            //Colided with a baddie
+        }   
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        string tag = other.gameObject.tag;
+
+        if (tag == "Fuel") {
+            Destroy(other.gameObject);
+            Debug.Log("Collected fuel");
+        }
+    }
+
+    void Rotate() {
 
         if (Input.GetKey(KeyCode.A)) {
+            rigidBody.freezeRotation = true;
             transform.Rotate(Vector3.forward * rotSpeed * Time.deltaTime);
         } else if (Input.GetKey(KeyCode.D)) {
+            rigidBody.freezeRotation = true;
             transform.Rotate(-Vector3.forward * rotSpeed * Time.deltaTime);
         }
+        else if(transform.rotation.z != 0) {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 0, 0),  Time.deltaTime);
+            transform.position = new Vector3(transform.position.x, transform.position.y, 0);
+        }
 
+        rigidBody.freezeRotation = false;
     }
+
 }
